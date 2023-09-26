2aa0c2c08c62c6252579e8592b7fc9bcdb7a5212
Slisken79 <essadak@yahoo.se>
2023-09-26 20:54:53 +0200

    text

--- testground/filer.py
+++ testground/filer.py
@@ -1,9 +1,10 @@
-# Skapa och skriv till en fil
-with open("./exempelfil.txt", "w") as fil:
+# Skapa och skriv till en fil i samma mapp
+with open("exempelfil.txt", "w") as fil:
     fil.write("Detta är en exempelfil.\nExtra text 2.")
 
 # Läsa från en fil
-with open("./exempelfil.txt", "r") as fil:
+with open("exempelfil.txt", "r") as fil:
     innehåll = fil.read()
     print("Filinnehåll:", innehåll)
+
  
