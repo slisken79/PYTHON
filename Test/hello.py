72219fd14a04c62c166b47bad6bede8ae881617b
Slisken79 <essadak@yahoo.se>
2023-09-27 21:28:49 +0200

    hem till /

--- Test/hello.py
+++ Test/hello.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/hem/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def hem():
     return render_template("index.html")
 
